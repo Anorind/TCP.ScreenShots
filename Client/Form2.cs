9119959d45d5e9725037f3332e20297b6f51780f
AnorX_zq13@student.itstep.org <AnorX_zq13@student.itstep.org>
2023-12-21 15:19:41 +0200

    Добавьте файлы проекта.

---
+++ Client/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TCP.ScreenShots.Client
+{
+    public partial class Form2 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread thread;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 8001);
+
+            client = new TcpClient();
+            client.Connect(ipEndPoint);
+            stream = client.GetStream();
+
+            thread = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        MessageBox.Show($"Отправка нового скриншота");
+                        using (Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
+                        {
+                            Graphics graphics = Graphics.FromImage(screenshot);
+                            graphics.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+
+                            using (MemoryStream memoryStream = new MemoryStream())
+                            {
+                                screenshot.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                                byte[] bytesToSend = memoryStream.ToArray();
+
+                                stream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                                MessageBox.Show($"Отправлено {memoryStream.Length} байт");
+                            }
+
+                            MessageBox.Show($"Скриншот отправлен");
+                        }
+                            Thread.Sleep(5000);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally 
+                { 
+                    stream.Close();
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stream.Close();
+            client.Close();
+            Application.Exit();
+        }
+    }
+}
+
---
+++ TCP.ScreenShots/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TCP.ScreenShots
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        private Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, 8001);
+                server.Start();
+
+                thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+                        NetworkStream stream = client.GetStream();
+
+                        Thread thread = new Thread(() =>
+                        {
+                            try
+                            {
+                                while (true)
+                                {
+                                    byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                                    int bytesRead = stream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+
+                                    using (MemoryStream memoryStream = new MemoryStream(bytesToRead, 0, bytesRead))
+                                    {
+                                        Bitmap screenshot = new Bitmap(memoryStream);
+                                        pictureBox1.Image = null; 
+                                        pictureBox1.Image = screenshot; 
+                                    }
+
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Error: " + ex.Message);
+                            }
+                            finally
+                            {
+                                client.Close();
+                            }
+                        });
+                        thread.IsBackground = true;
+                        thread.Start();
+                    }
+                });
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            server.Stop();
+            Application.Exit();
+        }
+    }
+}
